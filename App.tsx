88e01b8824bf83ca3d48c9e103a869949d493d5a
JulianaSoba <julianasobajava23@gmail.com>
2023-06-23 16:43:23 +0100

    :fire: add changes on first screen

--- App.tsx
+++ App.tsx
@@ -1,17 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import MyLogo from "./assets/logo.svg"
+import MyImage from "./assets/mainImage.svg"
+
+
 
 export default function App() {
 
-  const myImage = require('./assets/mainImage.png')
-  const myLogo = require('./assets/logo.png')
   return (
     <View style={styles.container}>
-      <Image source={myLogo} style={styles.logo}/>
-      <Image source={myImage} style={styles.image}/>
+      <MyLogo style={styles.logo}/>
+      <MyImage style={styles.mainImage}/>
       <Text style={styles.text}>All the recipes {'\n'} on your fingertips</Text>
       <Text style={styles.smtext}>Cooking on Thermomix made easy</Text>
-      <Button title='Let start' color='#fff'/>
+      <TouchableOpacity style={styles.button} onPress={() => {
+      }}>
+        <Text style={styles.buttonText}>Lest's start</Text>
+      </TouchableOpacity>
+      
 
       <StatusBar style="auto" />
     </View>
@@ -26,26 +32,45 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     position:'relative'
   },
-  image:{
+  logo:{
+    position: 'absolute',
+    top: 80,
   },
+  mainImage:{
+    marginBottom:50,
+    width:150,
+    height:150,
+   },
   text:{
     fontWeight: '600',
-    fontSize:30,
+    fontSize:32,
     lineHeight:35,
-    marginTop:20,
+    marginTop:10,
     textAlign:'center',
     color:'#1C0F13',
   },
-  smtext:{
+
+smtext:{
 fontWeight: '400',
-fontSize: 12,
+fontSize: 14,
 lineHeight: 23,
 alignItems: 'center',
 textAlign: 'center',
 color: 'rgba(28, 15, 19, 0.9)',
   },
-  logo:{
+  button: {
+    width:280,
+    height:40,
+    backgroundColor: '#1C0F13', 
+    alignItems:'center',
+    justifyContent:'center',
+    borderRadius: 8,
+    color: '#FFFFFF',
     position: 'absolute',
-    top: 60,
-  }
+    bottom:70,
+  },
+  buttonText: {
+    color: 'white', 
+    textAlign: 'center',
+  },
 });
